c579b4e539e64eb3a032002cdacba042bf87ec6b
dalia ramos and Sara Hamilton <sara@>
2018-02-13 14:22:32 -0800

    add red condition for CalculateNickels test method

--- CoinCombination.Tests/ModelTests/CoinCombinationTest.cs
+++ CoinCombination.Tests/ModelTests/CoinCombinationTest.cs
@@ -14,11 +14,19 @@ namespace CoinCombination.Tests
       CoinCombinationGenerator testCoinCombination = new CoinCombinationGenerator();
       Assert.AreEqual(2, testCoinCombination.CalculateQuarters(68));
     }
+
     [TestMethod]
     public void CoinCombination_CalculateDimes_Int()
     {
       CoinCombinationGenerator testCoinCombination = new CoinCombinationGenerator();
       Assert.AreEqual(1, testCoinCombination.CalculateDimes(18));
+    }
+
+    [TestMethod]
+    public void CoinCombination_CalculateNickels_Int()
+    {
+      CoinCombinationGenerator testCoinCombination = new CoinCombinationGenerator();
+      Assert.AreEqual(1, testCoinCombination.CalculateNickels(8));
     }
   }
 }
